926cf4eef9f442280902847d05e89451f6fc7b41
Jawwad Baig <jawwad.baig@digital.education.gov.uk>
2022-06-09 16:58:28 +0100

    #SVJ-717 Indesser service health checks are added

--- BEIS.HelpToGrow.Voucher.Web/Services/HealthCheck/IndesserHealthCheckService.cs
+++ BEIS.HelpToGrow.Voucher.Web/Services/HealthCheck/IndesserHealthCheckService.cs
@@ -1,12 +1,13 @@
 ﻿using BEIS.HelpToGrow.Voucher.Web.Config;
 using BEIS.HelpToGrow.Voucher.Web.Services.Connectors.Domain;
+using BEIS.HelpToGrow.Voucher.Web.Services.Eligibility;
 using FluentResults;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -17,25 +18,40 @@ namespace BEIS.HelpToGrow.Voucher.Web.Services.HealthCheck
         private readonly ILogger<CompanyHouseHealthCheckService> _logger;
         private readonly IIndesserHttpConnection<IndesserCompanyResponse> _indesserHttpConnection;
         private readonly IOptions<CompanyHouseHealthCheckConfiguration> _companyHouseHealthCheckOptions;
+        private readonly ICheckEligibility _eligibility;
 
-        public IndesserHealthCheckService(ILogger<CompanyHouseHealthCheckService> logger, 
-            IIndesserHttpConnection<IndesserCompanyResponse> indesserHttpConnection, 
-            IOptions<CompanyHouseHealthCheckConfiguration> companyHouseHealthCheckOptions)
+        public IndesserHealthCheckService(ILogger<CompanyHouseHealthCheckService> logger,
+            IIndesserHttpConnection<IndesserCompanyResponse> indesserHttpConnection,
+            IOptions<CompanyHouseHealthCheckConfiguration> companyHouseHealthCheckOptions, ICheckEligibility eligibility)
         {
             _logger = logger;
             _indesserHttpConnection = indesserHttpConnection;
             _companyHouseHealthCheckOptions = companyHouseHealthCheckOptions;
+            _eligibility = eligibility;
         }
 
         public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
         {
             var isHealthy = true;
 
+            StringBuilder indesserCheckErrors = new StringBuilder();
+
             var indesserCallResult = RunIndesserCheck(_companyHouseHealthCheckOptions.Value.CompanyNumber);
 
             if (indesserCallResult.IsFailed)
             {
+                indesserCheckErrors.Append(indesserCallResult.Errors[0].Message);
                 isHealthy = false;
+
+            } else { 
+
+                var eligibilityCalculation = _eligibility.Check(new Web.Models.Voucher.UserVoucherDto { }, indesserCallResult.Value);
+
+                if (eligibilityCalculation.IsFailed)
+                {
+                    indesserCheckErrors.Append(eligibilityCalculation.Errors[0].Message);
+                    isHealthy = false;
+                }
             }
 
             if (isHealthy)
@@ -45,7 +61,7 @@ namespace BEIS.HelpToGrow.Voucher.Web.Services.HealthCheck
 
             return
                 Task.FromResult(new HealthCheckResult(
-                    context.Registration.FailureStatus, "Indesser service health check failed."));
+                    context.Registration.FailureStatus, $"Indesser service health check failed. Its failed with {indesserCheckErrors}"));
         }
 
         private Result<IndesserCompanyResponse> RunIndesserCheck(string companyHouseNumber)
