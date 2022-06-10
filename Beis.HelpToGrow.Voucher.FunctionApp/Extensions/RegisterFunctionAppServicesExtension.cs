46bba70a3cbb43012dbffbe031973cb2f151fac7
paulc1983 <paul.cripps@gmail.com>
2022-06-10 10:56:09 +0100

    re added function app folder to fix case

---
+++ Beis.HelpToGrow.Voucher.FunctionApp/Extensions/RegisterFunctionAppServicesExtension.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+namespace Beis.HelpToGrow.Voucher.FunctionApp.Extensions
+{
+    public static class RegisterFunctionAppServicesExtension
+    {
+        public static IServiceCollection RegisterFunctionAppServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHttpClient();
+            services.AddOptions();
+            services.Configure<TokenReminderOptions>(options => configuration.Bind(options));
+            services.Configure<EncryptionSettings>(options => configuration.Bind(options));
+            services.Configure<EncryptionSettings>(options => configuration.Bind(options));
+            services.AddDbContext<HtgVendorSmeDbContext>(options => options.UseNpgsql(configuration["HelpToGrowDbConnectionString"]));
+            services.AddDataProtection().PersistKeysToDbContext<HtgVendorSmeDbContext>();
+
+            services.AddTransient<IEnterpriseRepository, EnterpriseRepository>();
+            services.AddTransient<ITokenRepository, TokenRepository>();
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddTransient<IVendorCompanyRepository, VendorCompanyRepository>();
+            services.AddTransient<IEmailClientService, EmailClientService>();
+            services.AddTransient<IVoucherGenerationService, VoucherGenerationService>();
+            services.AddTransient<IEncryptionService, EncryptionService>();
+            services.AddScoped<INotifyServiceSettings, NotifyServiceSettings>();
+
+            return services;
+        }
+    }
+}
