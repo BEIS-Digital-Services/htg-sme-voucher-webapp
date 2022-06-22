fbe8706a684980b602af7457fbc12dd43d7f8947
paulc1983 <paul.cripps@gmail.com>
2022-06-22 12:44:58 +0100

    re added commmon folder

---
+++ BEIS.HelpToGrow.Voucher.Web/Common/SessionRequiredActionFilter.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Beis.HelpToGrow.Voucher.Web.Common
+{
+    public class SessionRequiredActionFilter : IActionFilter
+    {
+        private readonly ISessionService _sessionService;
+
+        public SessionRequiredActionFilter(ISessionService sessionService)
+        {
+            _sessionService = sessionService;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.Controller is HomeController or SessionExpiredController or VerifyEmailAddressController or CancelVoucherController)
+            {
+                return;
+            }
+
+            if (!_sessionService.HasValidSession(context.HttpContext) && context.Controller is Controller controller)
+            {                
+                context.Result = controller.RedirectToAction("", "SessionExpired");              
+            }
+        }
+    }
+}
---
+++ BEIS.HelpToGrow.Voucher.Web/Common/SharedResultFilter.cs
@@ -0,0 +1,55 @@
+﻿
+namespace Beis.HelpToGrow.Voucher.Web.Common
+{
+    public class SharedResultFilter : IResultFilter
+    {
+        private readonly ISessionService _sessionService;
+        private readonly UrlOptions _options;
+
+        public SharedResultFilter(ISessionService sessionService, IOptions<UrlOptions> options)
+        {
+            _sessionService = sessionService;
+            _options = options.Value;
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            if (context.Controller is not Controller controller)
+            {
+                return;
+            }
+            SetupCookieSelection(controller);
+            SetupSatisfactionSurveyUrl(controller);
+        }
+
+        private void SetupSatisfactionSurveyUrl(Controller controller)
+        {
+            controller.ViewData["SatisfactionSurveyUrl"] = Urls.GetSatisfactionSurveyUrl(_options.LearningPlatformUrl);
+        }
+
+        public void OnResultExecuted(ResultExecutedContext context)
+        {            
+        }
+
+        private void SetupCookieSelection(Controller controller)
+        {
+            var actionName = controller.RouteData.Values["action"]?.ToString();
+            var controllerName = controller.RouteData.Values["controller"]?.ToString();
+            var userVoucherDto = _sessionService.Get<UserVoucherDto>("userVoucherDto", controller.HttpContext) ?? new UserVoucherDto { CookieBannerViewModel = new CookieBannerViewModel()};
+            
+            userVoucherDto.CookieBannerViewModel ??= new CookieBannerViewModel();
+            userVoucherDto.CookieBannerViewModel.ActionName = actionName;
+            userVoucherDto.CookieBannerViewModel.ControllerName = controllerName;
+            userVoucherDto.CookieBannerViewModel.PageName = actionName;
+
+            if (userVoucherDto.SelectedProduct?.product_id > 0 && userVoucherDto.SelectedProductType?.id > 0)
+            {
+                userVoucherDto.CookieBannerViewModel.ProductId = userVoucherDto.SelectedProduct.product_id;
+                userVoucherDto.CookieBannerViewModel.ProductType = userVoucherDto.SelectedProductType.id;
+            }
+            
+            _sessionService.Set("userVoucherDto", userVoucherDto, controller.HttpContext);
+            controller.ViewData["CookieBannerViewModel"] = userVoucherDto.CookieBannerViewModel;
+        }
+    }
+}
