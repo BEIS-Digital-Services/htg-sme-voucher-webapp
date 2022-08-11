9175fa01894300b948e80f7156b5cc7beade6d42
Jawwad Baig <jawwad.baig@digital.education.gov.uk>
2022-08-11 13:55:42 +0100

    #SVJ-845-Add-Enter applicant phone number page

---
+++ BEIS.HelpToGrow.Voucher.Web/Models/Applicant/PhoneNumberViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Beis.HelpToGrow.Voucher.Web.Models.Applicant
+{
+    public class PhoneNumberViewModel
+    {
+        [Required(ErrorMessage = "Enter your phone number")]
+        public string PhoneNumber { get; set; }
+    }
+}
