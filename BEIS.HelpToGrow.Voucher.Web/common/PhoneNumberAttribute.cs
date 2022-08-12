b5969150d6d361f29a5f3285b258cc4b4b4264be
Jawwad Baig <jawwad.baig@digital.education.gov.uk>
2022-08-12 08:46:04 +0100

    #SVJ-845-Added thephone number attribute with regex

--- BEIS.HelpToGrow.Voucher.Web/common/PhoneNumberAttribute.cs
+++ BEIS.HelpToGrow.Voucher.Web/common/PhoneNumberAttribute.cs
@@ -4,8 +4,7 @@
     {
         /// <summary>
         /// An actual UK phone number will start with 0 or +44 (the latter being the UK country code), or possibly just 44, followed by nine or ten digits.
-        /// Matches     +447222555555   | +44 7222 555 555 
-        /// Non-Matches(+447222)555555 | +44(7222)555555 
+        /// Matches +447222555555 | 07222555555 
         /// </summary>
         //@"^(?:0|\+?44)(?:\d\s?){9,14}$"
         public PhoneNumberAttribute() : base(@"^(?:0|\+?44)(?:\d\s?){9,14}$")
